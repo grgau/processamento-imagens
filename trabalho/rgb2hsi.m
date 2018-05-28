84ef51a38bed4f9437c9a15cd0f56dbad1fd77bf
grgau <pedro.ferracini@sjrp.unesp.br>
2018-05-28 19:01:03 -0300

    Funcao de conversao imagem RGB para HSI

---
+++ trabalho/rgb2hsi.m
@@ -0,0 +1,38 @@
+% Essa funcao recebe uma imagem RGB como entrada, realiza as equacoes de
+% conversao para HSI, e cria no diretorio atual a imagem convertida.
+
+function rgb2hsi(imagem_rgb)
+% Nome da imagem que sera usado para gerar imagem HSI
+[caminhoarquivo, nome, extensao] = fileparts(imagem_rgb);
+
+% Le a imagem RGB dada como entrada:
+imagem_rgb = imread(imagem_rgb);
+
+% Separando os canais da imagem RGB em r (vermelho), g (verde) e b (azul):
+rgb = im2double(imagem_rgb);
+r = rgb(:, :, 1);
+g = rgb(:, :, 2);
+b = rgb(:, :, 3);
+
+% Valor de H definido por:
+theta = acos((0.5*((r - g) + (r - b)))./((sqrt((r - g).^2 + (r - b).*(g - b))) + eps));
+if b > g
+    H = (2*pi)-theta;
+else
+    H = theta;
+end
+
+% Valor de S definido por:
+S = 1 - 3.* (min(min(r, g), b))./((r + g + b) + eps);
+
+% Valor de I definido por:
+I = (r + g + b)/3;
+
+% Criacao da imagem HSI:
+imagem_hsi = zeros(size(imagem_rgb));
+imagem_hsi(:,:,1) = H;
+imagem_hsi(:,:,2) = S;
+imagem_hsi(:,:,3) = I;
+
+imwrite(imagem_hsi, strcat(nome, '_hsi', extensao)); 
+end
